b747b1d88e7725fc436a79ee729fe6991be86f28
Dan Breczinski <pt2323@gmail.com>
2024-12-06 15:30:19 +0100

    reorganize week 5

--- programming2_class_5_demo/exceptions_banking_exercise_solution/exceptions_banking_exercise_solution/Exceptions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace exceptions_banking_exercise_solution
-{
-    public class InsufficientFundsException : ApplicationException
-    {
-        public InsufficientFundsException(string message) : base(message) { }
-    }
-}
